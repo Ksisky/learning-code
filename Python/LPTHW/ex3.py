e98a003b16c37e12b2c30c2e5f3d925f1dcb30b3
Ksisky <kevindsisk@gmail.com>
2016-01-10 12:24:26 -0500

    adding floating points

--- Python/LPTHW/ex3.py
+++ Python/LPTHW/ex3.py
@@ -1,18 +1,18 @@
 print "I will now count my chickens."
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 print "I will now count the eggs."
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7", 5 - 7
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7", 5.0 - 7.0
 
 print "Oh, that's why it's False."
 
