2e1d898a5b0551bdd7fab95542c1742e7bff4370
Ksisky <kevindsisk@gmail.com>
2016-01-11 20:30:51 -0500

    New exercises

---
+++ Python/LPTHW/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Kevin Sisk'
+my_age = 13
+my_height = 63
+my_weight = 85
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair= 'Brown'
+inches = 63 % 12
+feet = 63 / 12
+
+print "Lets talk about %s." % my_name
+print "He's %d feet, %d inches tall." % (feet, inches)
+print "He's %d pounds heavy." % my_weight
+print "He's got %s eyes and %s hair." %(my_eyes, my_hair)
+print "His teeth are usually %s" % my_teeth
+
+print "If I add %d, %d, and %d, I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
