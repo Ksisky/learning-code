aa53b61dbe5e0f7c2c9fcb07fd8bed482acd14bb
Ksisky <kevindsisk@gmail.com>
2016-01-16 20:20:36 -0500

    spelling difference for module
    changed capitalization for module

--- Python/lib/srcstr.py
+++ Python/lib/srcstr.py
@@ -2,5 +2,5 @@ import string
 #A little function I found off of StackExchange.
 #Returns true or false depending whether word is in string.
 #Credit:AMACB
-def isWordIn(word, string):
+def wordsearch(word, string):
     return not(string.find(word) == -1)
