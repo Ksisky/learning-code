1bfcdd09c00a7c8ecdf7d022b896a27bc87a13b1
Ksisky <kevindsisk@gmail.com>
2016-01-18 12:15:53 -0500

    New exercise
    Exercise 19

---
+++ Python/LPTHW/ex19.py
@@ -0,0 +1,8 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+	print("You have %d cheeses!" % cheese_count)
+	print("You have %d boxes of crackers!" % boxes_of_crackers)
+	print("Man, that's enough for a party!")
+	print("Get a blanket.\n")
+	
+	
+print("We can just give the function numbers ")
