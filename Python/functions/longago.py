eb23df5e74fdedeff40e8089b279ce4b2c4079ba
Ksisky <kevindsisk@gmail.com>
2016-01-24 22:01:35 -0500

    Minor Changes

--- Python/functions/longago.py
+++ Python/functions/longago.py
@@ -1,2 +1,5 @@
-def longago(tuple):
+def timesince(tuple):
 	import time
+	ctime = time.time() #Returns current time as SSE
+	ptime = time.mktime(tuple) #Turns the past time into a SSE
+	dtime = ctime - ptime #Difference in time.
