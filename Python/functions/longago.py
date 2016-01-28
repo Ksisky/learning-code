83ad6d6d716016d90052860a95e01b533adade22
Ksisky <kevindsisk@gmail.com>
2016-01-28 17:49:51 -0500

    More minor changes

--- Python/functions/longago.py
+++ Python/functions/longago.py
@@ -1,5 +1,11 @@
+import time
 def timesince(tuple):
 	import time
 	ctime = time.time() #Returns current time as SSE
+	ttime = time.gmtime() #Returns current time as tuple.
 	ptime = time.mktime(tuple) #Turns the past time into a SSE
-	dtime = ctime - ptime #Difference in time.
+	dtime = ctime - ptime #SSE Difference in time.
+	year1 = int(time.strftime("%Y", ttime))
+	year2 = int(time.strftime("%Y", tuple))
+	yeard = year1 - year2
+	
