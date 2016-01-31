2d247ff41a5c2779ae16541381f81b8bffce26b7
Ksisky <kevindsisk@gmail.com>
2016-01-31 11:30:42 -0500

    Finished editing ex19.py

--- Python/LPTHW/ex19.py
+++ Python/LPTHW/ex19.py
@@ -6,3 +6,19 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 	
 	
 print("We can just give the function numbers ")
+cheese_and_crackers(20, 30)
+
+
+print("Or, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+print("We can even do math inside too:")
+cheese_and_crackers(20 + 30, 5 + 6)
+
+
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
