66fcf7803ee2eb8ae74ad334a90650574c24578a
Ksisky <kevindsisk@gmail.com>
2016-02-13 16:47:42 -0500

    New files.

--- Python/LPTHW/ex25.py
+++ Python/LPTHW/ex25.py
@@ -1,12 +1,12 @@
 def break_words(stuff):
-	words = stuff.split()
+	words = stuff.split(' ')
 	return words
 	
 def sort_words(words):
 	return sorted(words)
 	
 def print_first_word(words):
-	word =  word.pop(0)
+	word =  words.pop(0)
 	print word
 	
 def print_last_word(words):
