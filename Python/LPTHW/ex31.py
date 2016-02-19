8de69b05132c8fd981aaf24b48b5d6cbf6187479
Ksisky <kevindsisk@gmail.com>
2016-02-18 22:48:59 -0500

    One and a half exercises.

---
+++ Python/LPTHW/ex31.py
@@ -0,0 +1,12 @@
+print "You enter a dark room with two doors. Do you go through door #1 or door #2?"
+
+door = raw_input('> ')
+
+if door == "1":
+	print "There is a giant bear here eating cheese cake. What do you do?"
+	print "1. Take the cake."
+	print "2. Scream at the bear."
+	
+	bear = raw_input('> ')
+	
+	i
